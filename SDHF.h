207251fe082c297a0f2da2b0d0fcd42f4ecd0fa9
sllllr-cpj <2997708711@qq.com>
2025-02-06 11:05:17 +0800

    Add files via upload

---
+++ SDHF.h
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<Windows.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+#include<assert.h>
+#include<errno.h>
+#define EPS 1e-9
+
+typedef struct Student {
+
+    char name[20];
+    char sex[10];
+    size_t age;
+
+}STU;
+
+void function();
+
+void reverse(char* p);
+
+void Ñî»ÔÈý½Ç();
+
+int compar(const void* a, const void* b);
